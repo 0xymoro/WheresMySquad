3189e8beee502273bb9d8d70840d4011a417ec98
Richard Lin <yifengl@stanford.edu>
2019-12-05 20:23:02 -0800

    [agent] Make sure each agent's movement is deterministic

--- CS238Project/Assets/Scripts/AgentBehavior.cs
+++ CS238Project/Assets/Scripts/AgentBehavior.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class AgentBehavior : MonoBehaviour
 {
-    private const int SEED = 1337;
+    public int SEED = 0;
     private const int UPDATE_FREQ = 30;
     private const float FORCE_UPDATE_MAG = 30f;
+    Random.State oldState;
 
     int updateCounter = 0;
 
@@ -15,6 +16,7 @@ public class AgentBehavior : MonoBehaviour
     void Start()
     {
         Random.InitState(SEED);
+        oldState = Random.state;
     }
 
     // Update is called once per frame
@@ -22,9 +24,12 @@ public class AgentBehavior : MonoBehaviour
     {
         if (updateCounter % UPDATE_FREQ == 0)
         {
+            // Make sure other calls to Random in other scipts doesn't mess up the deterministic random here
+            Random.state = oldState;
             //Uniform distrib centered around 0, range is [-0.5, 0.5)
             float noiseX = Random.Range(-FORCE_UPDATE_MAG, FORCE_UPDATE_MAG);
             float noiseY = Random.Range(-FORCE_UPDATE_MAG, FORCE_UPDATE_MAG);
+            oldState = Random.state;
             GetComponent<Rigidbody>().AddForce(new Vector2(noiseX, noiseY));
         }
         updateCounter++;
